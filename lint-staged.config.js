5b349e55992586b9f089d22d8bd14fd178aeb194
Brian Pooe <bria.method@gmail.com>
2023-06-16 13:39:58 +0200

    feat: removed if check for release

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '{libs,tools,.github}/**/*.{ts,js,json,md,html,css,scss,yml,yaml}': [
+  '{libs,apps, tools, .github}/**/*.{ts,js,json,md,html,css,scss,yml,yaml}': [
     'nx affected --target typecheck --uncommitted',
     'nx affected --target lint --uncommitted --fix true',
     'nx affected --target test --uncommitted',
