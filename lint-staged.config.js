e1299e99312b4e2d440fde097b82c7a0a332b0e2
Brian Pooe <bria.method@gmail.com>
2023-06-16 13:35:27 +0200

    feat: added husky and removed commitizen

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   '{libs,tools,.github}/**/*.{ts,js,json,md,html,css,scss,yml,yaml}': [
+    'nx affected --target typecheck --uncommitted',
     'nx affected --target lint --uncommitted --fix true',
     'nx affected --target test --uncommitted',
     'nx format:write --uncommitted --libs-and-apps'
