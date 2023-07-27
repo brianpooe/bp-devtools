7df2654d23bd75581e3458564d0262fcc99009a9
Brian Pooe <bria.method@gmail.com>
2023-07-27 19:39:30 +0200

    feat: added demo app for angular responsive table

---
+++ apps/ngx-responsive-table-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'bp-root',
+  template: `<h1>Welcome ngx-responsive-table</h1> `,
+  styles: ['']
+})
+export class AppComponent {}
